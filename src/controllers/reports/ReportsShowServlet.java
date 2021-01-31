87ee39bafc911ff69a7f90b9f6439ca2aae08956
teru09310931 <takoyaki7943329@gmail.com>
2021-01-31 15:37:29 +0900

    Add Report show

--- src/controllers/reports/ReportsShowServlet.java
+++ src/controllers/reports/ReportsShowServlet.java
@@ -10,21 +10,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Employee;
 import models.Report;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class ReportsEditServlet
+ * Servlet implementation class ReportsShowServlet
  */
-@WebServlet("/reports/edit")
-public class ReportsEditServlet extends HttpServlet {
+@WebServlet("/reports/show")
+public class ReportsShowServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReportsEditServlet() {
+    public ReportsShowServlet() {
         super();
     }
 
@@ -38,14 +37,10 @@ public class ReportsEditServlet extends HttpServlet {
 
         em.close();
 
-        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
-        if(r != null && login_employee.getId() == r.getEmployee().getId()) {
-            request.setAttribute("report", r);
-            request.setAttribute("_token", request.getSession().getId());
-            request.getSession().setAttribute("report_id", r.getId());
-        }
+        request.setAttribute("report", r);
+        request.setAttribute("_token", request.getSession().getId());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
         rd.forward(request, response);
     }
 
